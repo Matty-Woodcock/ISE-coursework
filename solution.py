Added Weighted Classes And Seeded Testing And Fixed Memory Leakage

solution.py
@@ -1,35 +1,80 @@
 import pandas as pd
 import numpy as np
 import torch
+import re
 
-from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
+from transformers import DistilBertTokenizer, DistilBertForSequenceClassification, logging
 from torch.utils.data import TensorDataset, DataLoader
 from torch.optim import AdamW   
-
 from sklearn.model_selection import train_test_split
+from sklearn.utils.class_weight import compute_class_weight
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_curve, auc
+from datetime import datetime
+
+def remove_html(text):
+    """Remove HTML tags using a regex."""
+    html = re.compile(r'<.*?>')
+    return html.sub(r'', text)
+
+def remove_emoji(text):
+    """Remove emojis using a regex pattern."""
+    emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags
+                               u"\U00002702-\U000027B0"
+                               u"\U000024C2-\U0001F251"  # enclosed characters
+                               "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', text)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu") #Runs on GPU or CPU if no GPU available
-print(f"Using {device}")
+print(f"Using {device}\n")
+
+logging.set_verbosity_error()
 
 tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
 
-for project in ["pytorch"]:#, "tensorflow", "keras", "incubator-mxnet", "caffe"]:
+#projects = ["pytorch", "tensorflow", "keras", "incubator-mxnet", "caffe"]
+projects = ["caffe"]
+
+for project in projects:
     df = pd.read_csv(f"data/{project}.csv")
-    df = df.drop(columns=["Unnamed: 0"])
+    df = df.drop(columns=["Unnamed: 0"]) #TO DO: This is ID?
 
     df["text"] = ( #TO DO: Potentially add more columns
         df["Title"].fillna("") + " " +
         df["Body"].fillna("")
     )
 
+    # for i in range(len(df)):
+    #     df["text"][i] = remove_html(df["text"][i])
+    #     df["text"][i] = remove_emoji(df["text"][i])
+
+    df["text"] = df["text"].apply(remove_html)
+    df["text"] = df["text"].apply(remove_emoji)
+
+    # lengths = df["text"].apply(lambda x: len(tokenizer.encode(x)))
+    # print(f"Average tokens: {lengths.mean():.0f}")
+    # print(f"Max tokens: {lengths.max()}")
+    # print(f"90th percentile: {lengths.quantile(0.9):.0f}")
+
     X = df["text"]
     y = df["class"]
 
     accuracies, precisions, recalls, f1s, aucs = [], [], [], [], []
 
-    for seed in range(1):
-        model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased") #TO DO: Slow?
+    n_runs = 30
+
+    for seed in range(n_runs):
+        run_start_time = datetime.now()
+        print(f"Project: {project}, Repeat: {seed + 1}/{n_runs}")
+
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
+        np.random.seed(seed)
+
+        model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels=2) #TO DO: Slow? Does num_labels work?
         model = model.to(device)
         optimizer = AdamW(model.parameters(), lr=2e-5)
 
@@ -40,6 +85,10 @@ for project in ["pytorch"]:#, "tensorflow", "keras", "incubator-mxnet", "caffe"]
             stratify=y,
             random_state=seed
         )
+
+        class_weights = compute_class_weight(class_weight='balanced', classes=np.unique(y_train), y=y_train)
+        class_weights_tensor = torch.tensor(class_weights, dtype=torch.float).to(device)
+        loss_function = torch.nn.CrossEntropyLoss(weight=class_weights_tensor)
 
         encoded_train = tokenizer(list(X_train), return_tensors="pt", padding=True, truncation=True, max_length=512)
 
@@ -58,8 +107,8 @@ for project in ["pytorch"]:#, "tensorflow", "keras", "incubator-mxnet", "caffe"]
                 batch_labels = batch_labels.to(device)
 
                 optimizer.zero_grad() #Reset gradients for each batch
-                batch_outputs = model(input_ids=batch_input_ids, attention_mask=batch_attention_mask, labels=batch_labels)
-                batch_loss = batch_outputs.loss
+                batch_outputs = model(input_ids=batch_input_ids, attention_mask=batch_attention_mask)
+                batch_loss = loss_function(batch_outputs.logits, batch_labels) #Manually compute weighted loss
                 batch_loss.backward()
                 optimizer.step()
 
@@ -79,6 +128,21 @@ for project in ["pytorch"]:#, "tensorflow", "keras", "incubator-mxnet", "caffe"]
         false_positive_rates, true_positive_rates, thresholds = roc_curve(y_test, y_pred, pos_label=1)
         aucs.append(auc(false_positive_rates, true_positive_rates))
 
+        #Prevents memory leakage
+        del model
+        del optimizer
+        del train_dataset
+        del train_dataloader
+        del encoded_train
+        del encoded_test
+        del batch_outputs
+        del test_outputs
+        torch.cuda.empty_cache()
+
+        run_end_time = datetime.now()
+        run_duration = (run_end_time - run_start_time).total_seconds()
+        print(f"{run_duration:.1f} Seconds, VRAM Used: {torch.cuda.memory_allocated()/1024**2:.0f}MB")
+
     print(f"\nProject: {project}")
     print(f"Average Accuracy: {np.mean(accuracies):.4f}")
     print(f"Average Precision: {np.mean(precisions):.4f}")
@@ -86,4 +150,136 @@ for project in ["pytorch"]:#, "tensorflow", "keras", "incubator-mxnet", "caffe"]
     print(f"Average F1 Score:  {np.mean(f1s):.4f}")
     print(f"Average AUC Score:  {np.mean(aucs):.4f}")
 
-    print(np.unique(y_pred, return_counts=True))
+    print(f"{np.unique(y_pred, return_counts=True)}\n")
+
+#TO DO LIST
+#1. Add scheduler
+#3. Add deletes
+#2. Slow model / fast model
+
+# Project: pytorch
+# Average Accuracy: 0.8965
+# Average Precision: 0.7914
+# Average Recall:    0.7201
+# Average F1 Score:  0.7419
+# Average AUC Score:  0.7201
+
+# Project: pytorch
+# Average Accuracy: 0.8982
+# Average Precision: 0.7638
+# Average Recall:    0.7681
+# Average F1 Score:  0.7590
+# Average AUC Score:  0.7681
+
+#Added class weights - better
+
+# Project: pytorch
+# Average Accuracy: 0.8841
+# Average Precision: 0.7739
+# Average Recall:    0.7953
+# Average F1 Score:  0.7693
+# Average AUC Score:  0.7953
+
+# Project: pytorch
+# Average Accuracy: 0.8841
+# Average Precision: 0.7487
+# Average Recall:    0.8100
+# Average F1 Score:  0.7710
+# Average AUC Score:  0.8100
+
+#With cleaning
+
+# Project: pytorch
+# Average Accuracy: 0.8867
+# Average Precision: 0.7629
+# Average Recall:    0.7821
+# Average F1 Score:  0.7643
+# Average AUC Score:  0.7821
+
+#First all run - 5 runs per project
+
+# Project: pytorch
+# Average Accuracy: 0.8619
+# Average Precision: 0.7297
+# Average Recall:    0.8149
+# Average F1 Score:  0.7494
+# Average AUC Score:  0.8149
+# (array([0, 1]), array([188,  38]))
+
+# Project: tensorflow
+# Average Accuracy: 0.9275
+# Average Precision: 0.8809
+# Average Recall:    0.8986
+# Average F1 Score:  0.8862
+# Average AUC Score:  0.8986
+# (array([0, 1]), array([372,  75]))
+
+# Project: keras
+# Average Accuracy: 0.8876
+# Average Precision: 0.8283
+# Average Recall:    0.8441
+# Average F1 Score:  0.8337
+# Average AUC Score:  0.8441
+# (array([0, 1]), array([146,  55]))
+
+# Project: incubator-mxnet
+# Average Accuracy: 0.8852
+# Average Precision: 0.7676
+# Average Recall:    0.8617
+# Average F1 Score:  0.7897
+# Average AUC Score:  0.8617
+# (array([0, 1]), array([108,  47]))
+
+# Project: caffe
+# Average Accuracy: 0.8767
+# Average Precision: 0.7135
+# Average Recall:    0.6871
+# Average F1 Score:  0.6816
+# Average AUC Score:  0.6871
+# (array([0, 1]), array([80,  6]))
+
+#30 runs
+
+# Project: pytorch
+# Average Accuracy: 0.8724
+# Average Precision: 0.7458
+# Average Recall:    0.8004
+# Average F1 Score:  0.7572
+# Average AUC Score:  0.8004
+
+# Project: tensorflow
+# Average Accuracy: 0.9124
+# Average Precision: 0.8564
+# Average Recall:    0.8782
+# Average F1 Score:  0.8627
+# Average AUC Score:  0.8782
+
+# Project: keras
+# Average Accuracy: 0.8658
+# Average Precision: 0.8044
+# Average Recall:    0.8335
+# Average F1 Score:  0.8072
+# Average AUC Score:  0.8335
+
+# Project: incubator-mxnet
+# Average Accuracy: 0.9112
+# Average Precision: 0.8097
+# Average Recall:    0.8432
+# Average F1 Score:  0.8145
+# Average AUC Score:  0.8432
+
+# Project: caffe
+# Average Accuracy: 0.8550
+# Average Precision: 0.6894
+# Average Recall:    0.6763
+# Average F1 Score:  0.6604
+# Average AUC Score:  0.6763
+
+#30 runs, seeds 1-30
+
+# Project: caffe
+# Average Accuracy: 0.8058
+# Average Precision: 0.6610
+# Average Recall:    0.7049
+# Average F1 Score:  0.6379
+# Average AUC Score:  0.7049